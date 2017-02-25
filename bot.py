56669bb5ff88d8aec760eb21fa4f36d31748e882
bhq12 <bhq12@uclive.ac.nz>
2017-02-25 19:37:46 +1300

    add block dropping and erasing, flip y axis so my brain doesn't explode

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ GRID_WIDTH = 6
 FUTURE_BLOCK_ARRAY_SIZE = 8
 SKULL_BLOCK_CHAR = '0'
 EMPTY_CELL_CHAR = '.'
-MINIMUM_ADJACENT = 4
+MINIMUM_ADJACENT = 3
 
 #('.' = empty, '0' = skull block, '1' to '5' = colored block)
 
@@ -15,6 +15,10 @@ class Block:
 		self.x = x
 		self.y = y
 		self.colour = colour
+	def __str__(self):
+		return "x: " + str(self.x) + " y: " + str(self.y) + " c: " + self.colour
+	def __repr__(self):
+		return "x: " + str(self.x) + " y: " + str(self.y) + " c: " + self.colour
 	
 def read_inputs():
 	for i in range(FUTURE_BLOCK_ARRAY_SIZE):
@@ -25,7 +29,7 @@ def read_inputs():
 		row = input()
 		grid[i] = [None] * GRID_WIDTH
 		for j in range(len(row)):
-			grid[i][j] = Block(j,i,row[j])
+			grid[i][j] = Block(j,GRID_HEIGHT - 1 - i,row[j])
 			
 	opponent_score = int(input())
 	
@@ -33,7 +37,7 @@ def read_inputs():
 		row = input()
 		opponent_grid[i] = [None] * GRID_WIDTH
 		for j in range(len(row)):
-			opponent_grid[i][j] = Block(j,i,row[j])
+			opponent_grid[i][j] = Block(j,GRID_HEIGHT - 1 - i,row[j])
 	
 	for row in range(len(grid)):
 		for i in range(len(grid[row])):
@@ -41,28 +45,53 @@ def read_inputs():
 			if(block.colour != EMPTY_CELL_CHAR):
 				top_row[i] = block.colour
 				top_row_height[i] = row
+	#my mental model prefers higher rows -> higher grid index
+	grid.reverse()
+	opponent_grid.reverse()
 
-def drop_block(block, grid):
+def drop_block_into_grid(block, grid):
 	height = GRID_HEIGHT - 1
 	row = block.x
+	
+	while height > 0:
+		if grid[height][row].colour != EMPTY_CELL_CHAR:
+			grid[height + 1][row] = block
+			block.y = height + 1
+		height -= 1
+
+def drop_block_within_grid(block, grid):
+	height = block.y
+	row = block.x
+	grid[block.y][block.x] = Block(block.x, block.y, EMPTY_CELL_CHAR)
+	
 	while height > 0:
 		if grid[height][row].colour != EMPTY_CELL_CHAR:
 			block.y = height + 1
 		height -= 1
 
+def erase_block(block, grid):
+	block.colour = EMPTY_CELL_CHAR
+	if(block.y < GRID_HEIGHT - 1):
+		above_block = grid[block.y + 1][block.x]
+		drop_block_within_grid(above_block)
+
 def check_turn(grid):
 	visited = set()
-	blocks_cleared = 0
+	scoring_blocks = set()
 	for y in range(GRID_HEIGHT):
 		for x in range(GRID_WIDTH):
 			block = grid[y][x]
 			if block.colour != EMPTY_CELL_CHAR and block.colour != SKULL_BLOCK_CHAR:
-				same_neighbours, visited = find_same_neighbours(grid[y][x], grid, visited, 1)
-				if same_neighbours >= MINIMUM_ADJACENT:
-					blocks_cleared += same_neighbours
-	return blocks_cleared
+				same_neighbours = find_same_neighbours(block, grid)
+				if len(same_neighbours) >= MINIMUM_ADJACENT:
+					scoring_blocks = scoring_blocks.union(same_neighbours)
+	return scoring_blocks
 
-def find_same_neighbours(block, grid, visited, neighbour_count):
+def find_same_neighbours(block, grid):
+	visited, same_neighbours = find_same_neighbours(block, grid, set(), 1, {block})
+	return same_neighbours
+	
+def find_same_neighbours(block, grid, visited, same_neighbours):
 	#visited = set()
 	visited.add(block)
 	#neighbour_count = 0
@@ -71,9 +100,10 @@ def find_same_neighbours(block, grid, visited, neighbour_count):
 	
 	for neighbour in same_neighbours:
 		neighbour_count += 1
-		neighbour_count, visited = find_same_neighbours(neighbour, grid, visited, neighbour_count)
+		same_neighbours.add(neighbour)
+		visited, same_neighbours = find_same_neighbours(neighbour, grid, visited, same_neighbours)
 	
-	return neighbour_count, visited
+	return visited, same_neighbours
 		
 def get_all_neighbours(block, grid):
 	neighbours = []
@@ -123,8 +153,9 @@ while True:
 	print("top_row:" + str(top_row), file=sys.stderr)
 	print("top_row_height:" + str(top_row_height), file=sys.stderr)
 	# "x": the column in which to drop your blocks
-	
+
 	print("3-in-a-row-blocks opponent: " + str(check_turn(opponent_grid)), file=sys.stderr)
+	print(opponent_grid, file=sys.stderr)
 	position = find_shortest_neighbour(place)
 	
 	for i in range(len(top_row)):
